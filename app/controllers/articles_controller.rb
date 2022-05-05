2ffae2995afb060ad7942ffc41502ab9cc3c3a0c
David Wertheimer <werthds@gmail.com>
2022-05-05 19:48:47 -0400

    add ability to edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,13 +12,27 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
-      flash[:notice] = "Article created sucessfully."
+      flash[:notice] = "Article sucessfully created."
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated sucessfully."
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
